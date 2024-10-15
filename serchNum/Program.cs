35559b963ca9d4f9155ef607fbd4131eee61d68c
Loli admin <Loli admin@DESKTOP-IUG32E2>
2024-10-15 20:55:01 +0300

    pre release 1

--- serchNum/Program.cs
+++ serchNum/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace serchNum
+﻿using System.Globalization;
+
+namespace serchNum_pre
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, my and maybe world!");
+            Console.WriteLine("enter n ");
+            String inp = Console.ReadLine();
+            bool apruvde = int.TryParse(inp, out int N);
+            if (apruvde == true)
+            {
+                if (20 < N && N < 35)
+                {
+                    Console.WriteLine("n - в нашем диапозоне ");
+                }
+                else
+                {
+                    Console.WriteLine("это левое число ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("не балуйся другими типами данных ");
+                Main();
+            }
         }
     }
 }
